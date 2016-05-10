dacfa30f2355ed7613268956952d81aefff56d3c
Joe Buhlig <joe@joebuhlig.com>
2016-05-10 16:32:27 -0500

    Custom list for topics

--- plugin.rb
+++ plugin.rb
@@ -12,28 +12,21 @@ register_asset "javascripts/departure-board.js"
 enabled_site_setting :departure_board_enabled
 
 after_initialize do
-  Discourse.top_menu_items.push(:departure_board)
-  Discourse.filters.push(:departure_board)
+  Discourse.top_menu_items.push(:departure)
+  Discourse.filters.push(:departure)
 
   require_dependency 'list_controller'
   class ::ListController
-    def departure_board
-      unless SiteSetting.departure_board_enabled
-        render nothing: true, status: 404
-      end
+    def departure
+      user = list_target_user
       list_opts = build_topic_list_options
-      target_user = fetch_user_from_params(include_inactive: current_user.try(:staff?))
-      list = generate_list_for("departure_board", target_user, list_opts)
-      list.more_topics_url = url_for(construct_url_with(:next, list_opts))
-      list.prev_topics_url = url_for(construct_url_with(:prev, list_opts))
+      list_opts[:per_page] = 10
+      list = TopicQuery.new(user, list_opts).public_send("list_latest")
       respond_with_list(list)
     end
   end
 
-  require_dependency 'topic_query'
-  class ::TopicQuery
-    def list_departure_board
-      create_list(:latest, {}, latest_results)
-    end
+  Discourse::Application.routes.append do
+    get "departure" => "list#departure"
   end
 end
